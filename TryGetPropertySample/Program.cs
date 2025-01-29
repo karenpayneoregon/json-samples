993353934e31cf2a0167663e3d70e9ca1444d3a3
Karen Payne <paynekaren@comcast.net>
2025-01-29 09:53:33 -0800

    :small_orange_diamond: Add TryGetPropertySample project and initial setup
    - Added new project "TryGetPropertySample" to solution.
    - Updated solution configuration for new project.
    - Added `people.json` with sample data.
    - Added `appsettings.json` with connection strings.
    - Added global using directives in `GlobalUsings.cs`.
    - Created `Program.cs` demonstrating `TryGetProperty`.
    - Added `readme.md` for project documentation.
    - Created `TryGetPropertySample.csproj` with dependencies.
    - Added images `command_2.png` and `Console1.png`.
    - Updated `csharp.ico` with new binary data.
    - Added `using` directives and `Init` method in `Program.cs`.
    
    by Karen

---
+++ TryGetPropertySample/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.Json;
+
+namespace TryGetPropertySample;
+
+internal partial class Program
+{
+    static void Main(string[] args)
+    {
+        var jsonString = MockedData();
+
+        double sum = 0;
+        int count = 0;
+
+        using (JsonDocument document = JsonDocument.Parse(jsonString))
+        {
+            JsonElement root = document.RootElement;
+            
+            JsonElement studentsElement = root.GetProperty("Students");
+
+            foreach (JsonElement student in studentsElement.EnumerateArray())
+            {
+                var name = student.GetProperty("Name").GetString();
+
+                if (student.TryGetProperty("Grade", out JsonElement gradeElement))
+                {
+                    AnsiConsole.MarkupLine($"[cyan]{name,-20} has a grade[/]");
+                    sum += gradeElement.GetDouble();
+                }
+                else
+                {
+                    AnsiConsole.MarkupLine($"[red]{name,-20} has no grade[/]");
+                    sum += 70;
+                }
+                count++;
+            }
+        }
+
+        double average = sum / count;
+        AnsiConsole.MarkupLine($"[white]Average grade :[/] [cyan]{average}[/]");
+
+        Console.ReadLine();
+    }
+
+    private static string MockedData()
+    {
+        var jsonString = 
+            /* lang=json*/
+            """
+            {
+              "Students": [
+                { "Name": "Alice", "Grade": 85 },
+                { "Name": "Bob", "Grade": 90 },
+                { "Name": "Charlie" },
+                { "Name": "David", "Grade": 78 },
+                { "Name": "Eve", "Grade": 92 },
+                { "Name": "Frank" }
+              ]
+            }
+
+            """;
+        return jsonString;
+    }
+}
---
+++ TryGetPropertySample/readme.md
@@ -0,0 +1,5 @@
+﻿# About
+
+Example for the `TryGetProperty` method.
+
+https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/use-dom#use-jsondocument
